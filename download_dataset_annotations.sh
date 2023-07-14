7cc8656b8ff563f185319cb856b1de8cf310f80b
Eduardo Leao <eduardo.leao@stoneage.com.br>
2023-07-14 09:16:54 -0300

    adding shell scripts

---
+++ download_dataset_annotations.sh
@@ -0,0 +1,3 @@
+wget https://gitlab.kitware.com/viratdata/viratannotations/-/archive/master/viratannotations-master.zip
+unzip viratannotations-master.zip -d data/virat/annotations
+rm viratannotations-master.zip
---
+++ download_dataset_videos.sh
@@ -0,0 +1,4 @@
+# First, go to this website and find the download button
+# https://data.kitware.com/#collection/56f56db28d777f753209ba9f/folder/56f581ce8d777f753209ca43
+unzip ~/Downloads/videos_original.zip -d data/virat/videos
+# rm videos_original.zip
